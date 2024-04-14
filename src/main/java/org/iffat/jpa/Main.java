b215ec410ca74acdf24508f634ed3fdb352cb81d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-14 08:45:32 +0700

    18. JPA in action

--- src/main/java/org/iffat/jpa/Main.java
+++ src/main/java/org/iffat/jpa/Main.java
@@ -1,8 +1,28 @@
 package org.iffat.jpa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import org.iffat.jpa.music.Artist;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		try (var sessionFactory = Persistence.createEntityManagerFactory("org.iffat.jpa");
+			 EntityManager entityManager = sessionFactory.createEntityManager()) {
+
+			var transaction = entityManager.getTransaction();
+			transaction.begin();
+			// entityManager.persist(new Artist("Muddy Water"));
+			// Artist artist = entityManager.find(Artist.class, 207);
+			Artist artist = new Artist(207, "Muddy Waters");
+			entityManager.merge(artist); // must use merge for constructor to persists db
+			System.out.println(artist);
+			// entityManager.remove(artist);
+			// artist.setArtistName("Muddy Waters"); // auto persists because transaction.commit()
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
