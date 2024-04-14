638b404f51f8a767ff2a3bfbd95d2c4c761f3676
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-14 17:26:06 +0700

    24. JPA Bonus Challenge: CriteriaBuilder Query with Joins

--- src/main/java/org/iffat/jpa/music/SongQuery.java
+++ src/main/java/org/iffat/jpa/music/SongQuery.java
@@ -3,6 +3,7 @@ package org.iffat.jpa.music;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.*;
 
 import java.util.List;
 
@@ -31,6 +32,15 @@ public class SongQuery {
 					});
 				});
 			});
+
+			System.out.printf("%-30s %-65s %s%n", "Artist", "Album", "Song Title");
+			System.out.printf("%1$-30s %1$-65s %1$s%n", dashedString);
+
+			var bMatches = getMatchedSongsBuilder(entityManager, "%" + word + "%");
+			bMatches.forEach(mat -> {
+				System.out.printf("%-30s %-65s %s%n",
+						(String) mat[0], (String) mat[1], (String) mat[2]);
+			});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -43,5 +53,21 @@ public class SongQuery {
 		var query = entityManager.createQuery(jpql, Artist.class);
 		query.setParameter(1, matchedValue);
 		return query.getResultList();
+	}
+
+	private static List<Object[]> getMatchedSongsBuilder(EntityManager entityManager, String matchedValue) {
+
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> query = criteriaBuilder.createQuery(Object[].class);
+
+		Root<Artist> root = query.from(Artist.class);
+		Join<Artist, Album> albumJoin = root.join("albums", JoinType.INNER);
+		Join<Album, Song> songJoin = albumJoin.join("playList", JoinType.INNER);
+
+		query.multiselect(root.get("artistName"), albumJoin.get("albumName"), songJoin.get("songTitle"))
+				.where(criteriaBuilder.like(songJoin.get("songTitle"), matchedValue))
+				.orderBy(criteriaBuilder.asc(root.get("artistName")));
+
+		return entityManager.createQuery(query).getResultList();
 	}
 }
