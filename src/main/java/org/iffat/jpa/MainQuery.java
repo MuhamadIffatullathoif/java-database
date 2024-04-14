20a4387931a87d1ee177ec6bfdf39d98094040eb
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-14 16:20:09 +0700

    22. JPA Queries, CriteriaBuilder and Native SQL

--- src/main/java/org/iffat/jpa/MainQuery.java
+++ src/main/java/org/iffat/jpa/MainQuery.java
@@ -4,9 +4,14 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.Tuple;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.iffat.jpa.music.Artist;
 
 import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainQuery {
@@ -30,6 +35,19 @@ public class MainQuery {
 							(String) a.get("names")
 					))
 					.forEach(System.out::println);
+
+			System.out.println("=".repeat(90));
+			Stream<Artist> artistStream = getArtistsBuilder(entityManager, "Bl%");
+			var map = artistStream
+					.limit(10)
+					.collect(Collectors.toMap(
+							Artist::getArtistName,
+							(a) -> a.getAlbums().size(),
+							Integer::sum,
+							TreeMap::new
+					));
+
+			map.forEach((key, value) -> System.out.println(key + " : " + value));
 			transaction.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -40,7 +58,7 @@ public class MainQuery {
 
 		// String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE :partialName";
 		// String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE ?1";
-		String jpql = "SELECT a FROM Artist a JOIN Album as album " +
+		String jpql = "SELECT a FROM Artist a JOIN albums album " +
 				"WHERE album.albumName LIKE ?1 OR album.albumName LIKE ?2";
 		var query = entityManager.createQuery(jpql, Artist.class);
 		query.setParameter(1, matchedValue);
@@ -52,6 +70,25 @@ public class MainQuery {
 
 		String jpql = "SELECT a.artistId id, a.artistName as names FROM Artist a WHERE a.artistName LIKE ?1";
 		var query = entityManager.createQuery(jpql, Tuple.class);
+		query.setParameter(1, matchedValue);
+		return query.getResultStream();
+	}
+
+	private static Stream<Artist> getArtistsBuilder(EntityManager entityManager, String matchedValue) {
+
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Artist> criteriaQuery = criteriaBuilder.createQuery(Artist.class);
+		Root<Artist> root = criteriaQuery.from(Artist.class);
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.like(root.get("artistName"), matchedValue));
+		criteriaQuery.orderBy(criteriaBuilder.asc(root.get("artistName")));
+		return entityManager.createQuery(criteriaQuery).getResultStream();
+	}
+
+	private static Stream<Artist> getArtistsSQL(EntityManager entityManager, String matchedValue) {
+
+		var query = entityManager.createNativeQuery("SELECT * FROM music.artists WHERE artist_name LIKE ?1",
+				Artist.class);
 		query.setParameter(1, matchedValue);
 		return query.getResultStream();
 	}
